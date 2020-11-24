3c2110bd23f8504f0274ee9ebccab7a40d536dab
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-24 11:26:04 +0530

    Added: Removed welcome message and display toss results

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,14 @@
-echo welcome
+#!/bin/bash
+
+HEADS=1
+TAILS=0
+
+toss=$((RANDOM%2))
+
+if [ $toss -eq $HEADS ]
+then
+    echo Heads is winner
+elif [ $toss -eq $TAILS ]
+then    
+    echo Tails is winner
+fi        
