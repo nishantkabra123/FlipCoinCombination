9c511b6620a4a2a44bcf7d359d53b7aea43a7693
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-24 13:02:49 +0530

    Added: stored singlet result and display percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,32 @@
-#!/bin/bash
+#!/bin/bash 
 
 HEADS=1
 TAILS=0
 
-toss=$((RANDOM%2))
+declare -A Singlet
+count=0
+percentage=0
 
-if [ $toss -eq $HEADS ]
-then
-    echo Heads is winner
-elif [ $toss -eq $TAILS ]
-then    
-    echo Tails is winner
-fi        
+while [ $count -lt 10 ]
+do 
+    toss=$((RANDOM%2))
+    if [ $toss -eq $HEADS ]
+    then
+        ((Singlet["H"]++))
+        echo Heads is winner
+    elif [ $toss -eq $TAILS ]
+    then    
+        ((Singlet["T"]++))
+        echo Tails is winner
+    fi
+    ((count++))           
+done  
+
+function getPercentage(){
+    percentage=$(( ${Singlet[$1]}*100/$count ))
+    echo $percentage
+}
+
+echo Keys in Singlet is ${!Singlet[@]}
+echo head wins in Singlet is ${Singlet["H"]} and percentage is `getPercentage H`
+echo tail wins in Singlet is ${Singlet["T"]} and percentage is `getPercentage T`
